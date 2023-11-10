0dfa1928b267b18ba79f14458fd8cb6faae93f6b
tobennaagubuche <agubuchetobe@gmail.com>
2023-11-10 00:29:01 +0000

    jnkj

--- lessons.js
+++ lessons.js
@@ -13,7 +13,55 @@ const lessons = [
       price: 25,
       spaces: 5,
     },
-    // Add more lesson objects...
+    {
+      id: 3,
+      subject: 'History',
+      location: 'Room C',
+      price: 15,
+      spaces: 5,
+    },
+    {
+      id: 4,
+      subject: 'Literature',
+      location: 'Room D',
+      price: 18,
+      spaces: 5,
+    },
+    {
+      id: 5,
+      subject: 'Art',
+      location: 'Studio A',
+      price: 30,
+      spaces: 5,
+    },
+    {
+      id: 6,
+      subject: 'Music',
+      location: 'Studio B',
+      price: 22,
+      spaces: 5,
+    },
+    {
+      id: 7,
+      subject: 'Physical Education',
+      location: 'Field',
+      price: 17,
+      spaces: 5,
+    },
+    {
+      id: 8,
+      subject: 'Computer Science',
+      location: 'Lab C',
+      price: 28,
+      spaces: 5,
+    },
+    {
+      id: 9,
+      subject: 'Biology',
+      location: 'Lab D',
+      price: 26,
+      spaces: 5,
+    },
   ];
   
   // Store lessons in localStorage
